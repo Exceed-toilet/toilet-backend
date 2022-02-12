e89ba68954a90d2ad586c319d854d40be7eeab7e
Kittison <kittison.j@ku.th>
2022-02-12 10:52:18 +0700

    post status

--- api_frontend.py
+++ api_frontend.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 from fastapi.encoders import jsonable_encoder
+from datetime import datetime
 
 
 client = MongoClient('mongodb://localhost', 27017)
@@ -13,3 +14,30 @@ db = client["toilet_project"]
 collection = db["user"]
 
 app = FastAPI()
+
+list_timestamp = []
+
+list_estimated = []
+
+
+class Toilet(BaseModel):
+    room_num: int
+    use_status: int
+
+
+@app.post('/toilet')
+def post_hardware(toilet: Toilet):
+    """"""
+    if toilet.use_status == 0:
+        list_timestamp.append(datetime.now())
+        return {
+            "result": "OK"
+        }
+    elif toilet.use_status == 1:
+        list_timestamp.append(datetime.now())
+        return {
+            "result": "OK"
+        }
+    return {
+        "result": "FAIL"
+    }
