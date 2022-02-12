3f00104f74bd4f573523b7ddc8787433ed6c39de
Kittison <kittison.j@ku.th>
2022-02-12 11:03:58 +0700

    get by room and post from hardware

--- api_frontend.py
+++ api_frontend.py
@@ -15,9 +15,9 @@ collection = db["user"]
 
 app = FastAPI()
 
-list_timestamp = []
-
-list_estimated = []
+list_enter_timestamp = []
+list_exit_timestamp = []
+list_delta_time = []
 
 
 class Toilet(BaseModel):
@@ -27,17 +27,32 @@ class Toilet(BaseModel):
 
 @app.post('/toilet')
 def post_hardware(toilet: Toilet):
-    """"""
+    """
+    use_status = 0 is no user
+    use_status = 1 is no user
+    """
     if toilet.use_status == 0:
-        list_timestamp.append(datetime.now())
+        list_exit_timestamp.append(datetime.now())
         return {
             "result": "OK"
         }
     elif toilet.use_status == 1:
-        list_timestamp.append(datetime.now())
+        list_enter_timestamp.append(datetime.now())
         return {
             "result": "OK"
         }
     return {
         "result": "FAIL"
     }
+
+
+@app.get('/toilet/by-room/{room_num}')
+def get_toilet(room_num: int):
+    list_result = list(collection.find({"room_number": room_num}, {"_id": 0}))
+    if len(list_result) != 0:
+        data = []
+        for result in list_result:
+            data.append(result)
+        return data
+    else:
+        raise HTTPException(404, f"Couldn't find toilet with room number: {room_num}'")
